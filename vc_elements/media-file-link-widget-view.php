9a897df6729fec6febcaa76df2ea3cbf123387e4
Rustam <rustamjon.akhmedov@zoftify.com>
2025-05-16 19:13:37 +0500

    Update nai-media-file-link-widget SVG icon for improved visual design and consistency in media file link presentation.

--- vc_elements/media-file-link-widget-view.php
+++ vc_elements/media-file-link-widget-view.php
@@ -1,7 +1,10 @@
 <div class="nai-media-file-link-block">
     <a href="<?php echo esc_url($file_url); ?>" class="nai-media-file-link" download>
         <span class="nai-media-file-link-icon" aria-hidden="true">
-            <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 3v12m0 0l-4-4m4 4l4-4" stroke="#7A8B7A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><rect x="4" y="17" width="16" height="4" rx="2" fill="#7A8B7A"/></svg>
+            <svg width="55" height="65" viewBox="0 0 55 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M27.5 10V41.25M27.5 41.25L16.0417 29.7917M27.5 41.25L38.9583 29.7917" stroke="#627A66" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                <rect x="9.16663" y="51.0417" width="36.6667" height="8.125" rx="4.0625" fill="#627A66"/>
+            </svg>
         </span>
         <span class="nai-media-file-link-content">
             <span class="nai-media-file-link-title"><?php echo esc_html($file_title); ?></span>
